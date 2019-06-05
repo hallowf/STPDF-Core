8dcb74b4f3c6bd4907cb89ee118cfadbef26d82c
hallowf <alexandre.cortegaca@gmail.com>
2019-06-05 16:00:08 +0100

    Changed deskew to automatically save the images on destination, fixed split checking on make_pdf, added verify_copy_size to handle multiple file copies

--- converter.py
+++ converter.py
@@ -23,7 +23,21 @@ class Converter(object):
             self.file_number += len(files)
 
 
+    def __del__(self):
+        for handle in self.images:
+            self.images[handle].close()
+
+    # checks how many files are there to copy over
+    def verify_copy_size(self):
+        if self.file_number >= 1000:
+            yield _("Found many file to copy, this might take a while")
+        else:
+            for line in self.copy_images():
+                yield line
+
+
     def copy_images(self):
+        yield "%s: %i" % (_("Files to copy"), self.file_number)
         for root, dirs, files in os.walk(self.source, topdown=False):
             for file in files:
                 extension = os.path.splitext(file)[1][1:].upper()
@@ -33,7 +47,9 @@ class Converter(object):
                     # Rotate the images first if deskew is true
                     img = Image.open(source_path)
                     if self.deskew:
-                        deskew_image(img,root,file)
+                        # TODO: If the images is to be deskewed it could be saved directly
+                        # in the des dir and continue the loop
+                        deskew_image(img,destination_dir,file)
                     else:
                         self.images[source_path] = img
                     # Check destination and copy files over
@@ -46,27 +62,30 @@ class Converter(object):
                     if not os.path.exists(destination_file):
                         shutil.copy2(source_path, destination_file)
 
-    def deskew_image(self, img, root, file):
-        source_path = os.path.join(root, file)
+    def deskew_image(self, img, dest, file):
+        dest_path = os.path.join(root, file)
         rotate = image_to_osd(img, output_type=Output.DICT)["rotate"]
         # This tells it to use the highest quality interpolation algorithm that it has available,
         # and to expand the image to encompass the full rotated size instead of cropping.
         # The documentation does not say what color the background will be filled with.
         # https://stackoverflow.com/a/17822099
-        self.images[source_path] = img.rotate(-rotate,resample=Image.BICUBIC, expand=True).save(source_path)
+        img.rotate(-rotate,resample=Image.BICUBIC, expand=True).save(dest_path)
 
     def make_pdf(self):
         # Get all image handles
-        sa = self.split_at
-        image_handles = [self.images[image] for image in self.images]
-        if len(image_handles) > sa:
-            # Mom's spaghetti ahead
-            image_handles = [image_handles[i * sa:(i + 1) * sa] for i in range((len(image_handles) + sa - 1) // sa)]
-            for list in image_handles:
-                first = list[0]
-                list.pop[0]
-                first.save("1.pdf", "PDF", resolution=90.0, save_all=True, append_images=image_handles)
+        if self.split:
+            sa = self.split_at
+            yield "%s %i" %  (_("Creating multiple PDFs splitting by:"), sa)
+            image_handles = [self.images[image] for image in self.images]
+            if len(image_handles) > sa:
+                # Mom's spaghetti ahead
+                image_handles = [image_handles[i * sa:(i + 1) * sa] for i in range((len(image_handles) + sa - 1) // sa)]
+                for list in image_handles:
+                    first = list[0]
+                    list.pop[0]
+                    first.save("1.pdf", "PDF", resolution=90.0, save_all=True, append_images=image_handles)
         else:
+            yield _("Creating a single pdf")
             # Remove the first and store it in a variable
             first = image_handles[0]
             image_handles.pop(0)
