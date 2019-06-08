6f2021171edcefcfbf9c5ebd12ca98b3bc54ec51
hallowf <alexandre.cortegaca@gmail.com>
2019-06-08 15:41:17 +0100

    Linting and code cleaning

--- converter.py
+++ converter.py
@@ -1,19 +1,21 @@
 # import the necessary packages
-import os, shutil
+import os
+import shutil
 from PIL import Image
 from pytesseract import image_to_osd, Output
 
 from pytesseract.pytesseract import TesseractNotFoundError
 
+
 class Converter(object):
     """docstring for Converter."""
 
-    def __init__(self,source,dest,split=(True,1),deskew=False, resolution=90.0):
+    def __init__(self, source, dest, split=(True, 1), *args, **kwargs):
         super(Converter, self).__init__()
         self.source = source
         self.dest = dest
-        self.deskew = deskew
-        self.resolution = resolution
+        self.deskew = kwargs.get("deskew", False)
+        self.resolution = kwargs.get("resolution", 90.0)
         self.images = {}
         self.counter = 0
         self.save_files = True
@@ -22,9 +24,8 @@ class Converter(object):
         self.split_at = at
         self.file_number = 0
         # Check how many files to copy
-        for root,dirs,files in os.walk(self.source):
+        for __, __, files in os.walk(self.source):
             self.file_number += len(files)
-
 
     def __del__(self):
         for handle in self.images:
@@ -32,7 +33,7 @@ class Converter(object):
 
     # checks how many files are there to copy over
     def verify_copy_size(self):
-        if self.file_number >= 1000:
+        if self.file_number >= 1000 and self.save_files:
             yield "Found too many files to copy, this is not implemented"
         else:
             for line in self.copy_images():
@@ -40,8 +41,8 @@ class Converter(object):
 
     # TODO: the images do not need to be copied neither saved if the user only wants a pdf
     def copy_images(self):
-        yield "%s: %i" % (_("Files to copy"), self.file_number)
-        for root, dirs, files in os.walk(self.source, topdown=False):
+        yield "%s: %i" % (_("Files Found"), self.file_number)
+        for root, __, files in os.walk(self.source, topdown=False):
             for file in files:
                 extension = os.path.splitext(file)[1][1:].upper()
                 source_path = os.path.join(root, file)
@@ -51,8 +52,13 @@ class Converter(object):
                     img = Image.open(source_path)
                     if self.deskew:
                         try:
-                            self.deskew_image(img,destination_dir,file)
-                        except (Exception,TesseractNotFoundError) as e:
+                            self.deskew_image(img, destination_dir, file)
+                            # continue because the image handle
+                            # is already in the list
+                            # and if save is true
+                            # it is already saved in the output dir
+                            continue
+                        except (Exception, TesseractNotFoundError) as e:
                             raise e
                     else:
                         self.images[source_path] = img
@@ -70,11 +76,14 @@ class Converter(object):
     def deskew_image(self, img, dest, file):
         dest_path = os.path.join(dest, file)
         rotate = image_to_osd(img, output_type=Output.DICT)["rotate"]
-        # This tells it to use the highest quality interpolation algorithm that it has available,
-        # and to expand the image to encompass the full rotated size instead of cropping.
-        # The documentation does not say what color the background will be filled with.
+        # This tells it to use the
+        # highest quality interpolation algorithm that it has available,
+        # and to expand the image to encompass the full rotated size
+        # instead of cropping.
+        # The documentation does not say
+        # what color the background will be filled with.
         # https://stackoverflow.com/a/17822099
-        img = img.rotate(-rotate,resample=Image.BICUBIC, expand=True)
+        img = img.rotate(-rotate, resample=Image.BICUBIC, expand=True)
         self.images[dest_path] = img
         if self.save_files:
             img.save(dest_path)
@@ -87,14 +96,16 @@ class Converter(object):
         else:
             if self.split:
                 sa = self.split_at
-                yield "%s %i" %  (_("Creating multiple PDFs splitting by:"), sa)
+                yield "%s %i" % (_("Creating multiple PDFs splitting by:"), sa)
                 if len(image_handles) > sa:
                     # Mom's spaghetti ahead
                     image_handles = [image_handles[i * sa:(i + 1) * sa] for i in range((len(image_handles) + sa - 1) // sa)]
                     for list in image_handles:
                         first = list[0]
                         list.pop[0]
-                        first.save("1.pdf", "PDF", resolution=90.0, save_all=True, append_images=image_handles)
+                        self.counter += 1
+                        name = "%i.pdf" % (self.counter)
+                        first.save(name, "PDF", resolution=90.0, save_all=True, append_images=image_handles)
             else:
                 yield _("Creating a single pdf")
                 # Remove the first and store it in a variable
--- custom_exceptions.py
+++ custom_exceptions.py
@@ -1,9 +1,10 @@
 class DirMissing(Exception):
     """docstring for CheatsMissing."""
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
 
 class OutDirNotEmpty(Exception):
     """docstring"""
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
