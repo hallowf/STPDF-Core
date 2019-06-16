f1dc578f1b446c37f7ba4093c0f24eb074250dd7
hallowf <alexandre.cortegaca@gmail.com>
2019-06-16 17:25:35 +0100

    Added an exception for TimedOut

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -25,3 +25,9 @@ class OutDirNotEmpty(Exception):
     """docstring"""
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
+
+
+class TimedOut(Exception):
+    """docstring"""
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
