3e2b5d9427000a49df45167c30f9d67070ade61a
hallowf <alexandre.cortegaca@gmail.com>
2019-06-21 18:58:38 +0100

    Updated language installation and fixed function for getting all the image handles, the images deskewed and not saved also get stored and used properly, added function for resize altough this function is only called on the make_pdf function

--- converter.py
+++ converter.py
@@ -38,10 +38,12 @@ class Converter(object):
         self.deskew = kwargs.get("deskew", False)
         self.resolution = kwargs.get("resolution", 90.0)
         self.installed_lang = kwargs.get("lang", None)
-        self.make_pdf = kwargs.get("make_pdf", True)
+        self.m_pdf = kwargs.get("make_pdf", True)
         self.save_files = kwargs.get("save_files", False)
+        self.resize = 10
         self.image_paths = []
         self.images = []
+        self.image_handles = None
         self.counter = 0
         self.stop_running = False
         split, at = split
@@ -53,10 +55,21 @@ class Converter(object):
         for __, __, files in os.walk(self.source):
             self.file_number += len(files)
         self.one_percent_files = self.file_number / 100
-        if self.installed_lang is None:
+        # Install language
+        if self.installed_lang == "en" or self.installed_lang is None:
             gettext.install("stpdf-core")
         else:
-            self.installed_lang.install()
+            modl = "%s_converter" % self.installed_lang
+            current_locale, encoding = locale.getdefaultlocale()
+            cl = current_locale.split("_")
+            if self.installed_lang != cl[0]:
+                current_locale = "%s_%s" % (self.installed_lang,
+                                            self.installed_lang.upper())
+            lang = gettext.translation(modl,
+                                       "locale",
+                                       [current_locale])
+            lang.install()
+        # Mute other loggers
         self.mute_other_loggers()
 
     def mute_other_loggers(self):
@@ -64,19 +77,19 @@ class Converter(object):
 
     # checks how many files are there to copy over
     def preprocess_all(self):
-        if not self.save_files and not self.make_pdf:
+        if not self.save_files and not self.m_pdf:
             yield _("Nothing to do, neither save files or make pdf is selected")
         else:
             if self.file_number >= 600 and self.save_files:
                 yield _("Found too many files to copy, this is not implemented yet")
             else:
-                for line in self.process_images():
+                for line in self.gather_images():
                     yield line
 
     # TODO: the images do not need to be copied neither saved if the user only wants a pdf
-    def process_images(self):
+    def gather_images(self):
         known_extensions = ["JPG", "PNG"]
-        yield _("Starting image processing")
+        yield _("Starting image gathering") + "\n"
         yield "%s: %i\n" % (_("Files Found"), self.file_number)
         for root, __, files in os.walk(self.source, topdown=False):
             if self.stop_running:
@@ -86,10 +99,10 @@ class Converter(object):
                 if self.stop_running:
                     break
                 self.file_counter += 1
-                if(round(self.file_counter % self.one_percent_files, 1) == 0.1):
-                    msg = "%s / %s %s.\n" % (str(self.file_counter),
-                                             str(self.file_number),
-                                             _("processed"))
+                if(round(self.file_counter % self.one_percent_files, 1) == 0.0):
+                    msg = "%i / %i %s.\n" % (self.file_counter,
+                                             self.file_number,
+                                             _("gathered"))
                     yield msg
                 extension = os.path.splitext(file)[1][1:].upper()
                 source_path = os.path.join(root, file)
@@ -107,22 +120,24 @@ class Converter(object):
                             # it is already saved in the output dir
                             continue
                         except (Exception, TesseractNotFoundError) as e:
-                            raise e
+                            self.stop_running = True
+                            msg = "%s: %s" % (_("Error occurred while deskewing image"), e)
+                            yield msg
                     else:
                         self.image_paths.append(source_path)
                     # Check destination and copy files over
                     if self.save_files:
                         if not os.path.exists(destination_dir):
                             os.mkdir(destination_dir)
-                        else:
-                            file_name = str(file) + "." + extension.lower()
-
+                        file_name = str(file) + "." + extension.lower()
                         destination_file = os.path.join(destination_dir, file_name)
                         if not os.path.exists(destination_file):
                             shutil.copy2(source_path, destination_file)
-        yield "Processing done"
+        yield _("Gathering done")
 
     # BUG: Some images get flipped sideways
+    # TODO: this is slow probably because of either opening the images,
+    # or processing them trough tesseract
     def deskew_image(self, source_path, dest, file):
         dest_path = os.path.join(dest, file)
         img = Image.open(source_path)
@@ -135,25 +150,53 @@ class Converter(object):
         # what color the background will be filled with.
         # https://stackoverflow.com/a/17822099
         img = img.rotate(-rotate, resample=Image.BICUBIC, expand=True)
-        print("image", img)
         if self.save_files:
-            self.image_paths.append(source_path)
+            self.image_paths.append(dest_path)
             img.save(dest_path)
         else:
-            img.close()
             self.images.append(img)
+        img.close()
+
+    def check_handles(self):
+        image_handles = [Image.open(image) for image in self.image_paths]
+        PIL_handles = None
+        if len(self.images) > 0:
+            PIL_handles = [Image.open(image) for image in self.images]
+        if PIL_handles is not None:
+            image_handles += PIL_handles
+        if len(image_handles) == 0:
+            return False
+        return image_handles
+
+    # resizes the images based on a percentage
+    def resize_images(self, image_handles):
+        yield _("Resizing images") + "\n"
+        index = 0
+        fl = len(image_handles)
+        l_l = fl / 100
+        for img in image_handles:
+            if round(index % l_l, 1) == 0.0:
+                msg = "%i / %i %s\n" % (index,
+                                        fl,
+                                        _("resized"))
+                yield msg
+            size = (img.size[0] / self.resize, img.size[1] / self.resize)
+            img.thumbnail(size, Image.ANTIALIAS)
+            image_handles[index] = img
+            index += 1
 
     # Function to generate pdf's
     def make_pdf(self):
-        image_handles = None
         # Get all image handles
-        if not self.save_files:
-            image_handles = [Image.open(image) for image in self.images]
-        else:
-            image_handles = [Image.open(image) for image in self.image_paths]
-        if len(image_handles) == 0:
-            yield _("Failed to obtain image handles something went wrong")
-        else:
+        image_handles = self.check_handles()
+        if self.resize is not None:
+            for line in self.resize_images(image_handles):
+                yield line
+        if image_handles is not False:
+            # now that we have the handles let's
+            # clean the class's stored handles to spare some memory
+            self.image_paths = []
+            self.images = []
             if self.split:
                 sa = self.split_at
                 yield "%s %i" % (_("Creating multiple PDFs splitting by:"), sa)
